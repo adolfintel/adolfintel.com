275bb86eced1fea6631d0152fde1ef5d4e27ad03
adolfintel <dosse91@paranoici.org>
2019-01-22 16:46:47 +0100

    Updated Highlight.js

--- AI_MINI/HLJS/langs/dart.js
+++ AI_MINI/HLJS/langs/dart.js
@@ -9,8 +9,17 @@ Category: scripting
 function (hljs) {
   var SUBST = {
     className: 'subst',
-    begin: '\\$\\{', end: '}',
-    keywords: 'true false null this is new super'
+    variants: [
+       {begin: '\\$[A-Za-z0-9_]+'}
+    ],
+  };
+
+  var BRACED_SUBST = {
+    className: 'subst',
+    variants: [
+       {begin: '\\${', end: '}'},
+    ],
+    keywords: 'true false null this is new super',
   };
 
   var STRING = {
@@ -32,25 +41,25 @@ function (hljs) {
       },
       {
         begin: '\'\'\'', end: '\'\'\'',
-        contains: [hljs.BACKSLASH_ESCAPE, SUBST]
+        contains: [hljs.BACKSLASH_ESCAPE, SUBST, BRACED_SUBST]
       },
       {
         begin: '"""', end: '"""',
-        contains: [hljs.BACKSLASH_ESCAPE, SUBST]
+        contains: [hljs.BACKSLASH_ESCAPE, SUBST, BRACED_SUBST]
       },
       {
         begin: '\'', end: '\'',
         illegal: '\\n',
-        contains: [hljs.BACKSLASH_ESCAPE, SUBST]
+        contains: [hljs.BACKSLASH_ESCAPE, SUBST, BRACED_SUBST]
       },
       {
         begin: '"', end: '"',
         illegal: '\\n',
-        contains: [hljs.BACKSLASH_ESCAPE, SUBST]
+        contains: [hljs.BACKSLASH_ESCAPE, SUBST, BRACED_SUBST]
       }
     ]
   };
-  SUBST.contains = [
+  BRACED_SUBST.contains = [
     hljs.C_NUMBER_MODE, STRING
   ];
 
